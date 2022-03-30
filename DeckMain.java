9477303ecb62a6c988d5ef3ed64eef7996d53f87
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-30 09:38:42 +0530

    Added : Initilize the deck

---
+++ DeckMain.java
@@ -0,0 +1,5 @@
+package deckOfCards;
+
+public class DeckMain {
+
+}
---
+++ DeckOfCards.java
@@ -0,0 +1,41 @@
+package deckOfCards;
+
+import java.util.Random;
+
+public class DeckOfCards {
+    static String[] Suits = {"Clubs","Diamonds","Hearts","Speades"};
+    static String[] Ranks = {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+    static String[][] deck = new String[Suits.length][Ranks.length];
+
+    public void cardCreation() {
+        for(int i=0;i<Suits.length;i++) {
+            for (int j = 0; j < Ranks.length; j++) {
+                deck[i][j] = Suits[i]+Ranks[j];
+            }
+        }
+    }
+
+    public void shuffle() {
+        Random random = new Random();
+        for (int i = 0; i < Suits.length; i++) {
+            for (int j = 0; j < Ranks.length; j++) {
+                int m = random.nextInt(i+1);
+                int n = random.nextInt(j+1);
+                String temp = deck[i][j];
+                deck[i][j] = deck[m][n];
+                deck[m][n] = temp;
+            }
+        }
+    }
+
+    public void distribute() {
+        for (int i = 0; i < 4; i++) {
+            System.out.println(" ");
+            System.out.println("Cards Recieved By Player "+(i+1)+ " are");
+            for (int j = 0; j < 9; j++) {
+                System.out.println("  "+deck[i][j]);
+            }
+        }
+    }
+}
+
