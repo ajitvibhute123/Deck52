69c55c6810ed3497b60170b2e9aa3315aa806aa1
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-30 09:44:39 +0530

    Added : Shuffle the Cards

--- DeckMain.java
+++ DeckMain.java
@@ -1,5 +1,11 @@
 package deckOfCards;
 
 public class DeckMain {
+    public static void main(String[] args) {
+        DeckOfCards card = new DeckOfCards();
 
+        card.cardCreation();
+        card.shuffle();
+        //card.distribute();
+    }
 }
